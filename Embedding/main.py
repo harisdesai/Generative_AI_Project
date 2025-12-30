fbc37cfd3b488e3ed846da540b228c85c4965bb7
Navanath Bhosale <navanathbhosle29@gmail.com>
2025-12-30 12:50:25 +0530

    Stored In Chroma DB

---
+++ Embedding/main.py
@@ -0,0 +1,9 @@
+import embedding
+import os   
+import chromadb
+import chromadb_store as cs 
+
+cs.store_db("C:/Users/navan/Desktop/Generative_AI_Project/Scraped_data/Internship_Scrap/sunbeam_internship_full.json")
+
+
+
