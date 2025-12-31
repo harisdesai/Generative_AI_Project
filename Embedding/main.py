96fd74133613d8e321c974c91c9bdfaddaec152b
harisdesai <harisdesai007@gmail.com>
2025-12-31 10:17:21 +0530

    frontend

--- Embedding/main.py
+++ Embedding/main.py
@@ -3,7 +3,4 @@ import os
 import chromadb
 import chromadb_store as cs 
 
-cs.store_db("C:\\Users\\haris\\.git\\GENAI\\Generative_AI_Project\\sunbeam_internship_full.json")
-
-
-
+# cs.store_db("C:\\Users\\haris\\.git\\GENAI\\Generative_AI_Project\\sunbeam_internship_full.json")
